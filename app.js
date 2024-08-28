77629ba01332c7ae0a72d37f762f621fc7a493ee
Kautilya Tiwari <160465716+Kautilyaprectice@users.noreply.github.com>
2024-08-29 03:45:34 +0530

    user table made

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('66cdae3c7fce0f7a959d9699')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('66cf9ac34de29ab68ba033f2')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,6 +34,20 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://kautilya:Kautilya@cluster0.xunre.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
   .then(result => {
+    User.findOne()
+      .then(user => {
+        if(!user){
+          const user = new User({
+            name: 'Amar',
+            email: 'amar@gmail.com',
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+        }
+      })
+    
     app.listen(3000);
     console.log('connected!');
   })
--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,8 @@ exports.postAddProduct = (req, res, next) => {
     title: title,
     price: price,
     description: description,
-    imageUrl: imageUrl
+    imageUrl: imageUrl,
+    // userId: req.user._id
   });
   product.save()
     .then(result => {
@@ -74,7 +75,9 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+  // .populate('userId', 'name')
     .then(products => {
+      // console.log(products)
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
